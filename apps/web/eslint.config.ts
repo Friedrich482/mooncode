4a2a4e7d93de9ff6e161cba214e72015fed57581
Friedrich482 <friedricht109@gmail.com>
2025-11-15 15:43:22 +0100

    fix: eslint web
    - fixed the issues of the eslint configuration of the web app by changing the `module` and `moduleResolution` options to `nodenext` in the `tsconfig.json`
    - updated the deps: next to `16.0.3`, react to `19.2.0` and eslint to `9.39.1`

--- apps/web/eslint.config.ts
+++ apps/web/eslint.config.ts
@@ -1,16 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import commonLintConfig from "@repo/eslint-config/lint";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...commonLintConfig,
+  ...nextVitals,
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
 ];
 
-export default eslintConfig;
+export default config;
