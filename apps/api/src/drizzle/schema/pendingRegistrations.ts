2cf1c2c3c5f2858153d73c16c892fd146ba616c6
Friedrich482 <friedricht109@gmail.com>
2025-12-09 01:09:29 +0100

    feat: attempts column
    - added an attempt column in the pending_registrations table similarly to the password_resets table
    - generated and ran migrations scripts

--- apps/api/src/drizzle/schema/pendingRegistrations.ts
+++ apps/api/src/drizzle/schema/pendingRegistrations.ts
@@ -3,6 +3,7 @@ import { varchar } from "drizzle-orm/pg-core";
 import { text } from "drizzle-orm/pg-core";
 import { pgTable } from "drizzle-orm/pg-core";
 import { timestamp } from "drizzle-orm/pg-core";
+import { integer } from "drizzle-orm/pg-core";
 import { ulid } from "ulid";
 
 import { PENDING_REGISTRATION_CODE_LENGTH } from "@repo/common/constants";
@@ -19,6 +20,7 @@ export const pendingRegistrations = pgTable("pending_registrations", {
   email: text("email").notNull().unique(),
   hashedPassword: text("hashed_password").notNull(),
   code: varchar("code", { length: PENDING_REGISTRATION_CODE_LENGTH }).notNull(),
+  attempts: integer().notNull().default(0),
   expiresAt: timestamp("expires_at")
     .notNull()
     .default(sql`now() + interval '30 minutes'`),
