97b223e36a382f80dbf808bcb30e0c6ccd1441fa
Friedrich482 <friedricht109@gmail.com>
2025-12-22 23:18:16 +0100

    fix: sql migrations

--- apps/api/drizzle/0004_groovy_weapon_omega.sql
+++ apps/api/drizzle/0004_groovy_weapon_omega.sql
@@ -1,9 +1,20 @@
 DO $$
 BEGIN
-    ALTER TABLE "users" DROP CONSTRAINT IF EXISTS "users_google_email_unique";
+    BEGIN
+        ALTER TABLE "users" DROP CONSTRAINT IF EXISTS "users_google_email_unique";
+    EXCEPTION
+        WHEN undefined_object THEN NULL;
+    END;
+
+    BEGIN
+        ALTER TABLE "users" DROP CONSTRAINT IF EXISTS "users_google_id_unique";
+    EXCEPTION
+        WHEN undefined_object THEN NULL;
+    END;
+
     ALTER TABLE "users" ADD CONSTRAINT "users_google_id_unique" UNIQUE("google_id");
+
 EXCEPTION
-    WHEN duplicate_object THEN NULL;
-    WHEN undefined_object THEN NULL;
-    WHEN unique_violation THEN NULL;
+    WHEN duplicate_object OR duplicate_table OR unique_violation THEN
+        NULL;
 END $$;
