066fac3015f9f99fd134d2e7325a12fad56b9158
Friedrich482 <friedricht109@gmail.com>
2026-01-27 01:54:46 +0100

    fix: popups
    - removed the vscode notifications inviting the user to login each time minute when he is logged out

--- apps/vscode-extension/src/utils/auth/getToken.ts
+++ apps/vscode-extension/src/utils/auth/getToken.ts
@@ -1,10 +1,7 @@
-import * as vscode from "vscode";
-
 import { getExtensionContext } from "@/extension";
 
 import setStatusBarAfterLogin from "../status-bar/setStatusBarAfterLogin";
 import setStatusBarItem from "../status-bar/setStatusBarItem";
-import login from "./login";
 import { setLoginContext } from "./loginContext";
 import parseJwtPayload from "./parseJwtPayload";
 
@@ -19,7 +16,6 @@ const getToken = async () => {
     await setLoginContext(false);
     setStatusBarItem({ type: "auth" });
 
-    await login();
     token = await context.secrets.get("authToken");
     return token;
   }
@@ -30,16 +26,6 @@ const getToken = async () => {
     await setLoginContext(false);
     setStatusBarItem({ type: "auth" });
 
-    const selection = await vscode.window.showInformationMessage(
-      "You are logged out. Please login",
-      "Login",
-    );
-
-    if (selection !== "Login") {
-      return undefined;
-    }
-
-    await login();
     token = await context.secrets.get("authToken");
   }
 
