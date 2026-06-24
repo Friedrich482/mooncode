b751eeb062b3276d7c9a1c01eb79488622112323
Friedrich482 <friedricht109@gmail.com>
2026-06-24 15:53:45 +0200

    test: getDaysOfPeriodStatsGroupedByMonths
    - added tests for the `getDaysOfPeriodStatsGroupedByMonths` function

--- apps/api/src/analytics/utils/general/get-days-of-period-stats-grouped-by-months.ts
+++ apps/api/src/analytics/utils/general/get-days-of-period-stats-grouped-by-months.ts
@@ -13,7 +13,9 @@ export const getDaysOfPeriodStatsGroupedByMonths = (
     { month: string; timeSpent: number; startDate: Date; endDate: Date }
   >();
   const lastEntry = data.at(-1);
-  if (!lastEntry) return [];
+  if (!lastEntry) {
+    return [];
+  }
 
   const endDate = new Date(lastEntry.date);
 
